4f73f9ff542dbd2ee484b5680747df882ec3438d
foyjunnesa <foyjunnesamayna@gmail.com>
2022-01-28 07:14:49 +0600

    all-done

--- src/pages/AddService/AddService.js
+++ src/pages/AddService/AddService.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './AddService.css';
 import { useForm } from "react-hook-form";
 import axios from 'axios';
+import Service from './../Home/Service/Service';
 
 const AddService = () => {
     const { register, handleSubmit, reset } = useForm();
     const onSubmit = data => {
         console.log(data);
-        axios.post('http://localhost:5000/services', data)
+        axios.post('https://sheltered-thicket-57277.herokuapp.com/services', data)
             .then(res => {
                 if (res.data.inserteId) {
                     alert('added successfully');
@@ -18,12 +19,12 @@ const AddService = () => {
 
     return (
         <div className='add-service'>
-            <h2>Please Add a Service</h2>
+            <h2 style={{ marginTop: "30px" }} className="text-center text-muted mb-5">Please Add a <span className="fw-bold" style={{ fontSize: "30px", color: "tomato" }}>Service</span> </h2>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <input {...register("name", { required: true, maxLength: 20 })} placeholder='Name' />
                 <input {...register("description")} placeholder='Description' />
                 <input {...register("img")} placeholder='Img url' />
-                <input type="submit" />
+                <input className='submit' type="submit" />
             </form>
         </div>
     );
